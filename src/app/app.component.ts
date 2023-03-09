aa28b9dc113c243b091eba2954848a2e6af3db59
ihsanemirkuzucu <ihsanemirkuzucu44@gmail.com>
2023-03-09 11:39:59 +0300

    Directive ile fotoğraf galerisi oluşturma

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pages';
+currentPage = 0;
+images = 
+[
+  {
+    title : 'Mercedes',
+    url : 'https://cdn-2.motorsport.com/images/mgl/2eAv3nb2/s1200/george-russell-mercedes-f1-w14-1.webp'
+  },
+  {
+    title : 'Ferrari',
+    url : 'https://cdn-8.motorsport.com/images/mgl/6b7kbre0/s1200/carlos-sainz-ferrari-sf-23-1.webp'
+  },
+  {
+    title : 'Redbull',
+    url : 'https://cdn-5.motorsport.com/images/mgl/2wBVDOW0/s1200/max-verstappen-red-bull-racing-1.webp'
+  },
+  {
+    title : 'Aston Martin',
+    url : 'https://cdn-3.motorsport.com/images/mgl/YpNZKOJ0/s1200/fernando-alonso-aston-martin-r-1.webp'
+  },
+  {
+    title : 'Haas',
+    url : 'https://cdn-2.motorsport.com/images/mgl/0L1DJKJ2/s1200/nico-hulkenberg-haas-vf-23-mak-1.webp'
+  }
+]
+
 }
